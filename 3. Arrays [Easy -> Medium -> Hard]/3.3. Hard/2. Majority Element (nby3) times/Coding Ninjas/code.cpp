0eea433283aa06e48c948a774b6ce2ed1cb6f52c
Jayesh Gaba <111695826+JayGaba@users.noreply.github.com>
2024-03-06 21:07:36 +0000

    message

---
+++ 3. Arrays [Easy -> Medium -> Hard]/3.3. Hard/2. Majority Element (nby3) times/Coding Ninjas/code.cpp
@@ -0,0 +1,17 @@
+//using hashmaps
+#include <bits/stdc++.h>
+vector<int> majorityElement(vector<int> v) {
+	// Write your code here
+	vector<int> ans;
+	map<int, int> mpp;
+	int n = v.size();
+	int mini = (int)(n/3)+1;
+	for(int i = 0; i < n; i++){
+		mpp[v[i]]++;
+		if(mpp[v[i]] == mini){
+			ans.push_back(v[i]);
+		}
+		if(ans.size() == 2)	break;
+	}
+	return ans;
+}
