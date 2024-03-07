9ffe38710d6bb5aa19fafecf436ff41a03ec9878
Jayesh Gaba <111695826+JayGaba@users.noreply.github.com>
2024-03-07 14:00:42 +0000

    message

--- 3. Arrays [Easy -> Medium -> Hard]/3.3. Hard/2. Majority Element (nby3) times/Coding Ninjas/code.cpp
+++ 3. Arrays [Easy -> Medium -> Hard]/3.3. Hard/2. Majority Element (nby3) times/Coding Ninjas/code.cpp
@@ -17,3 +17,34 @@ vector<int> majorityElement(vector<int> v) {
 }
 
 //optimized approach ->
+#include <bits/stdc++.h>
+vector<int> majorityElement(vector<int> v) {
+	// Write your code here
+	int cnt1 = 0, cnt2 = 0;
+	int el1 = INT_MIN, el2 = INT_MIN;
+	for(int i = 0; i < v.size(); i++){
+		if(cnt1 == 0 && el2!=v[i]){
+			cnt1 = 1;
+			el1 = v[i];
+		}
+		else if(cnt2 == 0 && el1!=v[i]){
+			cnt2 = 1;
+			el2 = v[i];
+		}
+		else if(v[i] == el1) cnt1++;
+		else if(v[i] == el2) cnt2++;
+		else{
+			cnt1--;
+			cnt2--;
+		}
+	}
+	vector<int> ls;
+	cnt1 = 0, cnt2 = 0;
+	for(int i = 0; i < v.size(); i++){
+		if(v[i] == el1) cnt1++;
+		if(v[i] == el2) cnt2++;
+	}
+	if(cnt1 > v.size()/3) ls.push_back(el1);
+    if(cnt2 > v.size()/3) ls.push_back(el2);
+    return ls;
+}
